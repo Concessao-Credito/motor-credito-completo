651800b84f0bb59f54e84acfa35cf1b85213b36d
Breno <brenohenriqueanacleto@gmail.com>
2024-11-11 21:39:29 -0300

    Commit Resilencia

--- src/main/java/com/challenge/motor_credito_completo/config/RabbitMQConfig.java
+++ src/main/java/com/challenge/motor_credito_completo/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package com.challenge.motor_credito_completo.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -27,10 +24,6 @@ public class RabbitMQConfig {
         return new Queue(CAPTACAO_COMPLETA_CONCUIDA_QUEUE, true);
     }
 
-    @Bean
-    public Queue motorCreditoP2ConcluidoQueue() {
-        return new Queue(MOTOR_CREDITO_P2_CONCLUIDO_QUEUE, true);
-    }
 
     @Bean
     public TopicExchange exchange() {
@@ -63,4 +56,43 @@ public class RabbitMQConfig {
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
+
+    @Bean
+    public Queue motorCreditoP2ConcluidoQueue() {
+        return QueueBuilder.durable(MOTOR_CREDITO_P2_CONCLUIDO_QUEUE)
+                .withArgument("x-dead-letter-exchange", "retry.exchange")
+                .withArgument("x-dead-letter-routing-key", "retry.queue")
+                .build();
+    }
+
+    @Bean
+    public Queue retryQueue() {
+        return QueueBuilder.durable("retry.queue")
+                .withArgument("x-message-ttl", 600000)
+                .withArgument("x-dead-letter-exchange", "appExchange")
+                .withArgument("x-dead-letter-routing-key", MOTOR_CREDITO_P2_CONCLUIDO_QUEUE)
+                .build();
+    }
+
+    @Bean
+    public DirectExchange retryExchange() {
+        return new DirectExchange("retry.exchange", true, false);
+    }
+
+    @Bean
+    public Binding bindingMainQueueToRetryExchange() {
+        return BindingBuilder
+                .bind(motorCreditoP2ConcluidoQueue())
+                .to(retryExchange())
+                .with("retry.queue");
+    }
+
+    @Bean
+    public Binding bindingRetryQueueToMainExchange() {
+        return BindingBuilder.bind(retryQueue())
+                .to(exchange())
+                .with(MOTOR_CREDITO_P2_CONCLUIDO_QUEUE);
+    }
+
+
 }
